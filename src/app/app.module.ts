972e7c8cedb61dc96dd91b934c12b9658c76c0d3
Erick Suarez <ericksuarezdev@gmail.com>
2020-03-04 17:02:02 -0600

    Basic template to start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+/** Angular Modules */
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
 
+/** Modules */
+import { TasksModule } from 'src/app/modules/tasks/tasks.module'
+
+/** Components */
 import { AppComponent } from './app.component';
+
+/** Ng Zorro */
+import { NZ_I18N, en_US } from 'ng-zorro-antd'
+
+/** Confign anglar i18n */
+import { registerLocaleData } from '@angular/common'
+import en from '@angular/common/locales/en';
+registerLocaleData(en)
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    TasksModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
