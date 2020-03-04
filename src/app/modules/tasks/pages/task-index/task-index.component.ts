4d6f943bd26ff84e95b9186f762a6bfac4740e55
Erick Suarez <ericksuarezdev@gmail.com>
2020-03-04 17:02:23 -0600

    mend

---
+++ src/app/modules/tasks/pages/task-index/task-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dol-task-index',
+  templateUrl: './task-index.component.html',
+  styleUrls: ['./task-index.component.less']
+})
+export class TaskIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
