407858489a56e440f331b2f0d2ba74116f7b7d8d
Erick Suarez <ericksuarezdev@gmail.com>
2020-03-05 01:44:09 -0600

    List

--- src/app/modules/tasks/tasks.module.ts
+++ src/app/modules/tasks/tasks.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 /** Ng Zorro Modules */
+import { NzListModule } from 'ng-zorro-antd/list'
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton'
 import { NzButtonModule } from 'ng-zorro-antd/button'
 
 /** Components */
 import { TaskIndexComponent } from './pages/task-index/task-index.component';
-import { TaskComponent } from './components/task/task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 
 
 @NgModule({
-  declarations: [TaskIndexComponent, TaskComponent],
+  declarations: [TaskIndexComponent, TaskListComponent],
   imports: [
     CommonModule,
-    NzButtonModule
+    NzButtonModule,
+    NzListModule,
+    NzSkeletonModule
   ],
   exports: [
     TaskIndexComponent
