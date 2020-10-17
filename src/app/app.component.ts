72711e89ef4de6e837b32aa0fbd4165db075d5b6
Erick Suarez <ericksuarezdev@gmail.com>
2020-10-17 18:11:30 -0500

    Create Agnular 10 app

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dodo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dodo-list-chrome-extension';
+}
