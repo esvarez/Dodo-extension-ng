6a86c3e178426d8d40be2f2e78b0f2df5250b74c
Erick Suarez <ericksuarezdev@gmail.com>
2020-10-18 23:00:23 -0500

    V2 design

--- src/app/modules/user/components/task-list/task-list.component.ts
+++ src/app/modules/user/components/task-list/task-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faCheck, faPen, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCircle } from '@fortawesome/free-regular-svg-icons'
 @Component({
   selector: 'dodo-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
+
+  faPen = faPen
+  faCheck = faCheck
+  faCircle = faCircle
+  faTimesCircle = faTimesCircle
 
   constructor() { }
 
